7278bc10dec1e9a632cdaab9cd7557bcfe5a4e87
Igor Chorazewicz <igor.chorazewicz@intel.com>
2023-02-06 18:09:43 +0000

    XXX

---
+++ test/test_c_cpp/test.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "test.h"
+
+void test_c() {
+    printf("TEST_C\n");
+}
---
+++ test/test_c_cpp/test.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: MIT
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+void test_c();
+
+#if defined(__cplusplus)
+}
+#endif
+
