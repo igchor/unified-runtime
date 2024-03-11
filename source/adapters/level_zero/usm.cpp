6e3899a3a118efc34cd1b462add01e82aa8a4899
Igor Chorążewicz <igor.chorazewicz@intel.com>
2024-03-11 13:42:15 -0700

    Bump UMF version and update integration code

--- source/adapters/level_zero/usm.cpp
+++ source/adapters/level_zero/usm.cpp
@@ -824,7 +824,7 @@ ur_usm_pool_handle_t_::ur_usm_pool_handle_t_(ur_context_handle_t Context,
 
   HostMemPool =
       umf::poolMakeUniqueFromOps(
-          &UMF_DISJOINT_POOL_OPS, std::move(MemProvider),
+          umfDisjointPoolOps(), std::move(MemProvider),
           &this->DisjointPoolConfigs.Configs[usm::DisjointPoolMemType::Host])
           .second;
 
@@ -835,7 +835,7 @@ ur_usm_pool_handle_t_::ur_usm_pool_handle_t_(ur_context_handle_t Context,
     DeviceMemPools.emplace(
         std::piecewise_construct, std::make_tuple(device),
         std::make_tuple(umf::poolMakeUniqueFromOps(
-                            &UMF_DISJOINT_POOL_OPS, std::move(MemProvider),
+                            umfDisjointPoolOps(), std::move(MemProvider),
                             &this->DisjointPoolConfigs
                                  .Configs[usm::DisjointPoolMemType::Device])
                             .second));
@@ -846,7 +846,7 @@ ur_usm_pool_handle_t_::ur_usm_pool_handle_t_(ur_context_handle_t Context,
     SharedMemPools.emplace(
         std::piecewise_construct, std::make_tuple(device),
         std::make_tuple(umf::poolMakeUniqueFromOps(
-                            &UMF_DISJOINT_POOL_OPS, std::move(MemProvider),
+                            umfDisjointPoolOps(), std::move(MemProvider),
                             &this->DisjointPoolConfigs
                                  .Configs[usm::DisjointPoolMemType::Shared])
                             .second));
@@ -858,7 +858,7 @@ ur_usm_pool_handle_t_::ur_usm_pool_handle_t_(ur_context_handle_t Context,
         std::piecewise_construct, std::make_tuple(device),
         std::make_tuple(
             umf::poolMakeUniqueFromOps(
-                &UMF_DISJOINT_POOL_OPS, std::move(MemProvider),
+                umfDisjointPoolOps(), std::move(MemProvider),
                 &this->DisjointPoolConfigs
                      .Configs[usm::DisjointPoolMemType::SharedReadOnly])
                 .second));
