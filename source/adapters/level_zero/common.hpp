1490bc27aaa320258c65e22ad446d7458857fa57
Igor Chorazewicz <igor.chorazewicz@intel.com>
2024-05-21 02:05:00 +0000

    [L0] add throwing variant of ZE2UR_CALL
    which allows to use L0 calls in constructors and other
    places where returning an error is not feasible.
    
    There is already a try/catch block for each function
    in libapi.cpp that will propagate the thrown status
    to the user.

--- source/adapters/level_zero/common.hpp
+++ source/adapters/level_zero/common.hpp
@@ -327,6 +327,14 @@ ur_result_t ze2urResult(ze_result_t ZeResult);
       return ze2urResult(Result);                                              \
   }
 
+// Perform traced call to L0 and throw in case of error
+#define ZE2UR_CALL_EXCEPT(ZeName, ZeArgs)                                      \
+  {                                                                            \
+    ze_result_t ZeResult = ZeName ZeArgs;                                      \
+    if (auto Result = ZeCall().doCall(ZeResult, #ZeName, #ZeArgs, true))       \
+      throw ze2urResult(Result);                                               \
+  }
+
 // Perform traced call to L0 without checking for errors
 #define ZE_CALL_NOCHECK(ZeName, ZeArgs)                                        \
   ZeCall().doCall(ZeName ZeArgs, #ZeName, #ZeArgs, false)
