bcfd27fcff3f60cd79cc32dae7d0d64564f69934
Igor Chorazewicz <igor.chorazewicz@intel.com>
2024-06-18 16:45:43 +0000

    [L0] Add unique (per-adapter) ids to ur_device_handle_t_
    With Id, it's easy to replace some of the unordered_maps (caches)
    with vectors - id can be used as index into a vector.

--- source/adapters/level_zero/device.hpp
+++ source/adapters/level_zero/device.hpp
@@ -219,4 +219,7 @@ struct ur_device_handle_t_ : _ur_object {
   ZeCache<struct ze_global_memsize> ZeGlobalMemSize;
   ZeCache<ZeStruct<ze_mutable_command_list_exp_properties_t>>
       ZeDeviceMutableCmdListsProperties;
+
+  // Global (per-adapter) Id of the device
+  size_t Id;
 };
