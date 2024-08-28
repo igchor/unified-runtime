cff382a0c861e9c88fa4788c1c93c86b7748ce64
Igor Chorazewicz <igor.chorazewicz@intel.com>
2024-08-28 16:13:54 +0000

    [Benchmark] use UR_ADAPTERS_FORCE_LOAD in benchmarks
    to enable testing L0 v2 adapter which cannot be selected
    by ONEAPI_SELECTOR

--- scripts/benchmarks/benches/velocity.py
+++ scripts/benchmarks/benches/velocity.py
@@ -9,6 +9,7 @@ from .result import Result
 from utils.utils import run, create_build_path
 import os
 import re
+from .options import options
 
 class VelocityBench:
     def __init__(self, directory):
@@ -22,6 +23,7 @@ class VelocityBase(Benchmark):
         self.bench_name = name
         self.bin_name = bin_name
         self.code_path = os.path.join(self.vb.repo_path, self.bench_name, 'SYCL')
+        self.adapter_path = os.path.join(options.ur_dir, 'build', 'lib', f"libur_adapter_{options.ur_adapter_name}.so")
 
     def download_deps(self):
         return
@@ -46,7 +48,7 @@ class VelocityBase(Benchmark):
         return []
 
     def extra_env_vars(self) -> dict:
-        return {}
+        return {"UR_ADAPTERS_FORCE_LOAD" : self.adapter_path}
 
     def parse_output(self, stdout: str) -> float:
         raise NotImplementedError()
