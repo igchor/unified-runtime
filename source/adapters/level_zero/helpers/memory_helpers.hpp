6a69c2aa227a32296a22501819bc00982a185693
Igor Chorazewicz <igor.chorazewicz@intel.com>
2024-09-25 22:45:03 +0200

    XXX

--- source/adapters/level_zero/helpers/memory_helpers.hpp
+++ source/adapters/level_zero/helpers/memory_helpers.hpp
@@ -21,3 +21,16 @@ bool maybeImportUSM(ze_driver_handle_t hTranslatedDriver,
                     ze_context_handle_t hContext, void *ptr, size_t size);
 
 ze_memory_type_t getMemoryType(ze_context_handle_t hContext, void *ptr);
+
+struct ze_rect_params_t {
+  uint32_t dstPitch;
+  size_t dstSlicePitch;
+  uint32_t srcPitch;
+  size_t srcSlicePitch;
+  ze_copy_region_t srcRegion;
+  ze_copy_region_t dstRegion;
+};
+
+ze_rect_params_t ur2zeRectParams(ur_rect_offset_t SrcOrigin, ur_rect_offset_t DstOrigin,
+    ur_rect_region_t Region, size_t SrcRowPitch, size_t DstRowPitch,
+    size_t SrcSlicePitch, size_t DstSlicePitch);
