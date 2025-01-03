141d699ff1355d0d96a6caa8395286c177b5e94b
Igor Chorazewicz <igor.chorazewicz@intel.com>
2025-01-03 21:41:11 +0000

    [L0 v2] implement support for images

--- source/adapters/level_zero/v2/memory.hpp
+++ source/adapters/level_zero/v2/memory.hpp
@@ -17,12 +17,26 @@
 
 using usm_unique_ptr_t = std::unique_ptr<void, std::function<void(void *)>>;
 
-struct ur_mem_handle_t_ : private _ur_object {
+struct ur_mem_handle_t_ : protected _ur_object {
+  ur_mem_handle_t_(ur_context_handle_t hContext, size_t size)
+      : hContext(hContext), size(size) {}
+  virtual ~ur_mem_handle_t_() = default;
+
+  inline ur_context_handle_t getContext() const { return hContext; }
+  inline ReferenceCounter &getRefCount() { return RefCount; }
+  inline size_t getSize() const { return size; }
+
+protected:
+  const ur_context_handle_t hContext;
+  const size_t size;
+};
+
+struct ur_mem_buffer_t : public ur_mem_handle_t_ {
   enum class device_access_mode_t { read_write, read_only, write_only };
 
-  ur_mem_handle_t_(ur_context_handle_t hContext, size_t size,
-                   device_access_mode_t accesMode);
-  virtual ~ur_mem_handle_t_() = default;
+  ur_mem_buffer_t(ur_context_handle_t hContext, size_t size,
+                  device_access_mode_t accesMode);
+  virtual ~ur_mem_buffer_t() = default;
 
   // Following functions should always be called under the lock.
 
@@ -40,20 +54,15 @@ struct ur_mem_handle_t_ : private _ur_object {
                std::function<void(void *src, void *dst, size_t)> memcpy) = 0;
 
   inline device_access_mode_t getDeviceAccessMode() const { return accessMode; }
-  inline ur_context_handle_t getContext() const { return hContext; }
-  inline ReferenceCounter &getRefCount() { return RefCount; }
 
-  virtual size_t getSize() const;
   virtual ur_shared_mutex &getMutex();
 
 protected:
   const device_access_mode_t accessMode;
-  const ur_context_handle_t hContext;
-  const size_t size;
 };
 
 // non-owning buffer wrapper around USM pointer
-struct ur_usm_handle_t_ : ur_mem_handle_t_ {
+struct ur_usm_handle_t_ : ur_mem_buffer_t {
   ur_usm_handle_t_(ur_context_handle_t hContext, size_t size, const void *ptr);
   ~ur_usm_handle_t_();
 
@@ -73,16 +82,16 @@ private:
 // Manages memory buffer for integrated GPU.
 // For integrated devices the buffer has been allocated in host memory
 // and can be accessed by the device without copying.
-struct ur_integrated_mem_handle_t : public ur_mem_handle_t_ {
+struct ur_integrated_buffer_handle_t : public ur_mem_buffer_t {
   enum class host_ptr_action_t { import, copy };
 
-  ur_integrated_mem_handle_t(ur_context_handle_t hContext, void *hostPtr,
-                             size_t size, host_ptr_action_t useHostPtr,
-                             device_access_mode_t accesMode);
+  ur_integrated_buffer_handle_t(ur_context_handle_t hContext, void *hostPtr,
+                                size_t size, host_ptr_action_t useHostPtr,
+                                device_access_mode_t accesMode);
 
-  ur_integrated_mem_handle_t(ur_context_handle_t hContext, void *hostPtr,
-                             size_t size, device_access_mode_t accesMode,
-                             bool ownHostPtr);
+  ur_integrated_buffer_handle_t(ur_context_handle_t hContext, void *hostPtr,
+                                size_t size, device_access_mode_t accesMode,
+                                bool ownHostPtr);
 
   void *
   getDevicePtr(ur_device_handle_t, device_access_mode_t, size_t offset,
@@ -110,19 +119,19 @@ struct host_allocation_desc_t {
 
 // Manages memory buffer for discrete GPU.
 // Memory is allocated on the device and migrated/copies if necessary.
-struct ur_discrete_mem_handle_t : public ur_mem_handle_t_ {
+struct ur_discrete_buffer_handle_t : public ur_mem_buffer_t {
   // If hostPtr is not null, the buffer is allocated immediately on the
   // first device in the context. Otherwise, the buffer is allocated on
   // firt getDevicePtr call.
-  ur_discrete_mem_handle_t(ur_context_handle_t hContext, void *hostPtr,
-                           size_t size, device_access_mode_t accesMode);
-  ~ur_discrete_mem_handle_t();
+  ur_discrete_buffer_handle_t(ur_context_handle_t hContext, void *hostPtr,
+                              size_t size, device_access_mode_t accesMode);
+  ~ur_discrete_buffer_handle_t();
 
   // Create buffer on top of existing device memory.
-  ur_discrete_mem_handle_t(ur_context_handle_t hContext,
-                           ur_device_handle_t hDevice, void *devicePtr,
-                           size_t size, device_access_mode_t accesMode,
-                           void *writeBackMemory, bool ownDevicePtr);
+  ur_discrete_buffer_handle_t(ur_context_handle_t hContext,
+                              ur_device_handle_t hDevice, void *devicePtr,
+                              size_t size, device_access_mode_t accesMode,
+                              void *writeBackMemory, bool ownDevicePtr);
 
   void *
   getDevicePtr(ur_device_handle_t, device_access_mode_t, size_t offset,
@@ -157,8 +166,8 @@ private:
                               size_t size);
 };
 
-struct ur_mem_sub_buffer_t : public ur_mem_handle_t_ {
-  ur_mem_sub_buffer_t(ur_mem_handle_t hParent, size_t offset, size_t size,
+struct ur_mem_sub_buffer_t : public ur_mem_buffer_t {
+  ur_mem_sub_buffer_t(ur_mem_buffer_t *hParent, size_t offset, size_t size,
                       device_access_mode_t accesMode);
   ~ur_mem_sub_buffer_t();
 
@@ -171,11 +180,38 @@ struct ur_mem_sub_buffer_t : public ur_mem_handle_t_ {
   void unmapHostPtr(void *pMappedPtr,
                     std::function<void(void *src, void *dst, size_t)>) override;
 
-  size_t getSize() const override;
   ur_shared_mutex &getMutex() override;
 
 private:
-  ur_mem_handle_t hParent;
+  ur_mem_buffer_t *hParent;
   size_t offset;
-  size_t size;
+};
+
+struct ur_mem_image_t : public ur_mem_handle_t_ {
+  ur_mem_image_t(ur_context_handle_t hContext, ur_mem_flags_t flags,
+                 const ur_image_format_t *pImageFormat,
+                 const ur_image_desc_t *pImageDesc, void *pHost);
+  ur_mem_image_t(ur_context_handle_t, const ur_image_format_t *pImageFormat,
+                 const ur_image_desc_t *pImageDesc, ze_image_handle_t zeImage,
+                 bool ownZeImage);
+
+  ze_image_handle_t getZeImage() const { return zeImage.get(); }
+
+  std::pair<ze_image_handle_t, ze_image_region_t>
+  getRWRegion(ur_rect_offset_t &origin, ur_rect_region_t &region,
+              size_t rowPitch, size_t slicePitch);
+
+  struct copy_desc_t {
+    std::pair<ze_image_handle_t, ze_image_region_t> src;
+    std::pair<ze_image_handle_t, ze_image_region_t> dst;
+  };
+
+  static copy_desc_t getCopyRegions(ur_mem_image_t &src, ur_mem_image_t &dst,
+                                    ur_rect_offset_t &srcOrigin,
+                                    ur_rect_offset_t &dstOrigin,
+                                    ur_rect_region_t &region);
+
+private:
+  v2::raii::ze_image_handle_t zeImage;
+  ZeStruct<ze_image_desc_t> zeImageDesc;
 };
