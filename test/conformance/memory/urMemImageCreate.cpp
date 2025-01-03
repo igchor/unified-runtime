8b8c0b321d8609bb73a8a938d0fd7505da5b88d6
Igor Chorazewicz <igor.chorazewicz@intel.com>
2025-01-03 21:41:11 +0000

    [L0 v2] implement support for images
    Use dynamic_cast to convert ur_mem_handle_t to
    buffer or image. This is not ideal, but buffer and image
    are pretty much separate entites that just happen to
    share the same handle type. There is no shared impl
    between buffer and image, expcept for a few lines in
    urMemGetInfo.
    
    Alternative to dynamic_cast and inheritance would be
    to just use std::variant, but this is problematic since
    ur_mem_buffer_t has subclasses and relies on polymorphic
    behavior. Variant would have to hold pointers to buffer/image
    adding one more indirection.

--- test/conformance/memory/urMemImageCreate.cpp
+++ test/conformance/memory/urMemImageCreate.cpp
@@ -203,7 +203,7 @@ TEST_P(urMemImageCreateTest, InvalidSize) {
                        uur::OpenCL{"Intel(R) UHD Graphics 770"});
 
   // This fail is specific to the "Multi device testing" ci job.
-  UUR_KNOWN_FAILURE_ON(uur::LevelZero{});
+  UUR_KNOWN_FAILURE_ON(uur::LevelZero{}, uur::LevelZeroV2{});
 
   uur::raii::Mem image_handle = nullptr;
 
@@ -234,7 +234,7 @@ TEST_P(urMemImageCreateTest, InvalidSize) {
 
 TEST_P(urMemImageCreateTest, InvalidImageDescStype) {
   // This fail is specific to the "Multi device testing" ci job.
-  UUR_KNOWN_FAILURE_ON(uur::LevelZero{});
+  UUR_KNOWN_FAILURE_ON(uur::LevelZero{}, uur::LevelZeroV2{});
 
   uur::raii::Mem image_handle = nullptr;
   ur_image_desc_t invalid_image_desc = image_desc;
@@ -335,7 +335,7 @@ TEST_P(urMemImageCreateWithHostPtrFlagsTest, Success) {
   UUR_KNOWN_FAILURE_ON(uur::OpenCL{"Intel(R) FPGA"});
 
   // This fail is specific to the "Multi device testing" ci job.
-  UUR_KNOWN_FAILURE_ON(uur::LevelZero{});
+  UUR_KNOWN_FAILURE_ON(uur::LevelZero{}, uur::LevelZeroV2{});
 
   uur::raii::Mem host_ptr_buffer = nullptr;
   ASSERT_SUCCESS(urMemImageCreate(context, UR_MEM_FLAG_ALLOC_HOST_POINTER,
